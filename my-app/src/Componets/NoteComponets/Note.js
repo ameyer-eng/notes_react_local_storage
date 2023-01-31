ebdee6da5fb73c8019b41b6ea5441a71be2012e2
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2023-01-30 21:09:59 -0600

    note input working

--- my-app/src/Componets/NoteComponets/Note.js
+++ my-app/src/Componets/NoteComponets/Note.js
@@ -1,7 +1,11 @@
 import React from "react";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+
+
+
 function Note() {
   return (
+
     <div className="note">
       <div className="note__body"></div>
       <div className="note__footer" style={{ justifyContent: "flex-end" }}>
